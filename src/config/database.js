111b0ccad0ff2bcb9cbc03238c51ef1c0d25f6f4
Ramathakur <rama1998thakur@gmail.com>
2025-06-11 23:57:33 +0530

    starting from scratch

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-connectDB = async () => {
-await mongoose.connect("mongodb+srv://rama1998thakur:vjCImbI2B5Y0zYpI@learnnode.qzsbi.mongodb.net/devTinder")
-}
-
-module.exports = connectDB
